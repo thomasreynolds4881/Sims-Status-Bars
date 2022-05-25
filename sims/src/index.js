8349dee77b493127357a14222a5b0759edba338c
thomasreynolds4881 <thomas.reynolds@drake.edu>
2022-05-25 02:33:19 -0500

    Added icons, color

--- sims/src/index.js
+++ sims/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import img_bladder from './imgs/bladder.png'
+import img_fun from './imgs/fun.png'
+import img_hunger from './imgs/hunger.png'
+import img_social from './imgs/social.png'
+import img_energy from './imgs/energy.png'
+import img_hygiene from './imgs/hygiene.png'
 
 const { useState, useEffect } = React;
 
@@ -21,25 +27,22 @@ const OpButton = ({val, onClick}) => {
 
 // Child Status Bar
 const StatusBar = (props) => {
-  const { curr } = props;
-
-  let curr_color;
-  if (curr > 66) curr_color = '#00FF00';
-  else if (curr > 33) curr_color = '#FFFF00';
-  else curr_color = '#FF0000';
-
+  const { curr, col } = props;
 
   const containerStyles = {
     height: 20,
     width: '150px',
     backgroundColor: "#e0e0de",
-    borderRadius: 50
+    borderRadius: 50,
+    marginTop: -40,
+    marginLeft: 10,
+    boxShadow: "1px 3px 1px #9E9E9E"
   }
 
   const fillerStyles = {
     height: '100%',
     width: `${curr}px`,
-    backgroundColor: `${curr_color}`,
+    backgroundColor: `${col}`,
     borderRadius: 'inherit',
     textAlign: 'right'
   }
@@ -64,9 +67,9 @@ const StatusItem = ({itemName, rate}) => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      let newVal = itemVal - rate/1;
+      let newVal = itemVal - rate/10;
       if (newVal > 0) {
-        setItemVal(itemVal => itemVal - rate/1);
+        setItemVal(itemVal => itemVal - rate/10);
       } else {
         setItemVal(itemVal => itemVal - itemVal);
       }
@@ -74,11 +77,35 @@ const StatusItem = ({itemName, rate}) => {
     return () => clearInterval(interval);
   }, [rate, itemVal]);
 
+  let curr_img;
+  if (itemName === "Bladder") curr_img = img_bladder;
+  else if (itemName === "Fun") curr_img = img_fun;
+  else if (itemName === "Hunger") curr_img = img_hunger;
+  else if (itemName === "Social") curr_img = img_social;
+  else if (itemName === "Energy") curr_img = img_energy;
+  else curr_img = img_hygiene;
+
+  let curr_color;
+  if (itemVal > 100) curr_color = '#00FF00';
+  else if (itemVal > 50) curr_color = '#FFFF00';
+  else curr_color = '#FF0000';
+
+  const imgStyles = {
+    backgroundColor: "#e0e0de",
+    borderRadius: 50,
+    marginLeft: -3,
+    background: `${curr_color}`,
+    boxShadow: "1px 3px 1px #9E9E9E"
+  }
+
   return (
     <div className = "item">
       <p>{itemName}</p>
       <div className="status">
-        <StatusBar curr={Math.floor(itemVal)} />
+        <img style={imgStyles} src={curr_img} alt="" />
+        <div>
+          <StatusBar curr={Math.floor(itemVal)} col={curr_color} />
+        </div>
       </div>
       <div className = "buttons">
         <OpButton val={10} onClick={changeVal}/>
